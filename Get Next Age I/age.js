c2604bbb879d8d3a700c61fb2a53f6893c92f0b7
MariDantas <61691909+MariDantas@users.noreply.github.com>
2023-07-31 19:01:53 -0300

    Delete Get Next Age I directory

--- Get Next Age I/age.js
+++
@@ -1,6 +0,0 @@
-/**
- * @param {string} age
- */
-export function getNextAge(age) {
-    return (Number.parseInt(age, 10)) + 1;
-}
--- Get Next Age I/index.js
+++
@@ -1,8 +0,0 @@
-import {getNextAge} from "./age.js";
-
-const age = document.querySelector("#your-age");
-const nextAge = document.querySelector("#next-age");
-
-age.addEventListener("keyup", () => {
-    nextAge.textContent = getNextAge(age.value);
-});
